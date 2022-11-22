65a0a4e7360bce5928b16befda7ef1757baa9229
turbotage <viktor.vigren@gmail.com>
2022-11-22 14:52:31 +0100

    started on new data, moved to single

--- test_viktor.m
+++ test_viktor.m
@@ -1,16 +1,30 @@
-%%
-rfmat = open('RF_MAT.mat').RF_MAT;
 
-frame = 5;
-Bmodes = sqrt(abs(hilbert(squeeze(rfmat(:,:,:)))));
+fn_STR = '181023_1311.mat';
+h = matfile(fn_STR);
+rfmatf = single(h.rf_data_set(:,:,1:500));
+%%
+%load DATA
+Bmodes = sqrt(abs(hilbert(squeeze(rfmatf(:,:,:)))));
+clear rfmatf
 shape = size(Bmodes)
 Bmodes_f = reshape(Bmodes, shape(1)*shape(2), shape(3));
+clear Bmodes
 
+%%
+% load TVI
+fn_STR = '181023_1311tvi.mat';
+h = matfile(fn_STR);
+tvif = single(h.TVI_MAT(:,:,1:500));
+tvif = sqrt(abs(hilbert(squeeze(tvif(:,:,:)))));
+
+%%
+
+%%
 [U,S,V] = svd(Bmodes_f, 'econ');
 
 Snew = S;
-Snew(100:end, 100:end) = 0;
-Snew(1:2,1:2) = 0;
+Snew(2:end, 25:end) = 0;
+Snew(1:4,1:4) = 0;
 
 Bmodes_fnew = U * Snew * V';
 Bmodes_new = reshape(Bmodes_fnew, shape(1), shape(2), shape(3));
@@ -21,45 +35,46 @@ Bmodes_new = reshape(Bmodes_fnew, shape(1), shape(2), shape(3));
 %animate_stuff(Bmodes_new);
 %%
 
-disp('Hello World');
-draw_pic(Bmodes, Bmodes_new);
 
-function animate_stuff(mat)
-    %movie(Bmodes, 1, 10);
-    Q = size(mat,3);
-    W = mat(:,:,1);
-    h = pcolor(W);
-    set(h, 'EdgeColor', 'none');
-    drawnow();
-    pause(0.3);
-    for K = 2:Q
-       W = mat(:,:,K);
-       set(h, 'CData', W);
-       drawnow limitrate;
-       %pause(0.1);
-       %disp(K);
-    end
-end
+disp('Hello World');
+draw_pic(tvif, Bmodes_new);
 
 function draw_pic(mat1, mat2)
+    lowi = 0.0
+    highi = 0.1
+
     figure(1);
     Q = size(mat1,3);
     W1 = mat1(:,:,1);
+    W1 = mat2gray(W1);
+    %W1 = imadjust(W1, [0,1],[]);
+
     W2 = mat2(:,:,2);
+    W2 = mat2gray(W2);
+    %W2 = imadjust(W2, [0,1],[]);
+
+
     subplot(1,2,1);
-    img1 = imagesc(W1);
-    colormap gray;
+    img1 = imshow(W1, [lowi, highi]);
+    axis('square')
+
     subplot(1,2,2);
-    img2 = imagesc(W2);
-    colormap gray;
+    img2 = imshow(W2, [lowi, highi]);
+    axis('square')
     
+    pause(2);
     for K = 2:Q
         W1 = mat1(:,:,K);
+        W1 = mat2gray(W1);
+        W1 = imadjust(W1, [0,1],[]);
         W2 = mat2(:,:,K);
+        W2 = mat2gray(W2);
+        W2 = imadjust(W2, [0,1],[]);
         set(img1, 'CData', W1);
         set(img2, 'CData', W2);
         %drawnow limitrate;
         drawnow();
+        caxis([0,1])
         pause(0.02);
         disp(K);
     end
